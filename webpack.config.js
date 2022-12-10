4581308b5e8442c303949611c38ecd6fb87c4a36
Kalinka George <xitowzys@gmail.com>
2022-12-10 11:29:40 +0800

    🎉 First commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        app: './js/app.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+      },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+              { from: "assets", to: "assets" },
+            ],
+        })
+    ],
+	devServer: {
+		static: path.join(__dirname, 'src'),
+		historyApiFallback: true,
+		port: 3000,
+		client: {
+			overlay: {
+				errors: true,
+				warnings: false,
+			},
+		},
+		// open: {
+		// 	app: {
+		// 		name: 'google-chrome',
+		// 	},
+		// },
+		hot: true,
+	},
+}
